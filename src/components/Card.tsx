0caa682e12765dcbe6abf290c29d75797bc8c36c
Vivian De La Vega Bellido <vivian.delavega.bellido@gmail.com>
2026-09-04 13:31:48 -0500

    Create Card component with props

---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+// const oldRender = () => {
+//   return <div>Old UI</div>;
+// }
+
+export const Card = (p1: string, p2: number, p3: boolean, p4: string, p5: any, p6: number) => {
+  return <div>{p1}</div>;
+};
